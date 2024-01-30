229e73995ba4da5a67bad2a5ae02cf80be6b3241
Murzel <marcel-sebastian-heinrich@outlook.de>
2024-01-30 21:35:48 +0100

    691

--- Program.cs
+++ Program.cs
@@ -4,33 +4,34 @@ using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
+using System.Reflection;
 
 namespace DSharpBot
 {
 	static class Program
-    {
-        public static DateTimeOffset CreatedAt { get; } = DateTimeOffset.Now;
+	{
+		public static DateTimeOffset CreatedAt { get; } = DateTimeOffset.Now;
 
-        public static Configuration Config { get; set; } = ConfigFileService.Config;
+		public static Configuration Config { get; set; } = ConfigFileService.Config;
 
 		static void Main(string[] args)
-        {
-            _ = CreatedAt;
+		{
+			_ = CreatedAt;
 
 			Program
-			    .CreateHost()
-                .GetAwaiter()
-                .GetResult();
-        }
+				.CreateHost()
+				.GetAwaiter()
+				.GetResult();
+		}
 
-        static async Task CreateHost()
-        {
-            var discord = new DiscordClient(new DiscordConfiguration()
-            {
-                Token = Config.Bot.Token,
-                TokenType = TokenType.Bot,
-                Intents = DiscordIntents.All
-            });
+		static async Task CreateHost()
+		{
+			var discord = new DiscordClient(new DiscordConfiguration()
+			{
+				Token = Config.Bot.Token,
+				TokenType = TokenType.Bot,
+				Intents = DiscordIntents.All
+			});
 
 			var commands = discord.UseCommandsNext(new CommandsNextConfiguration()
 			{
@@ -39,30 +40,57 @@ namespace DSharpBot
 
 			#region KEKW Smoking
 			discord.MessageReactionAdded += async (sender, args) =>
-            {
-                if( args.Message.Reactions.Where(x => x.Emoji.Name == "🚬").Any())
-                {
+			{
+				if (args.Message.Reactions.Where(x => x.Emoji.Name == "🚬").Any())
+				{
 					await args.Message.CreateReactionAsync(DiscordEmoji.FromName(sender, ":smoking:"));
-                }
+				}
 
-                await Task.CompletedTask;
-            };
+				await Task.CompletedTask;
+			};
+			#endregion
+
+			#region 691
+			List<(ulong user, DateTimeOffset until)> banned = new();
+
+			discord.MessageCreated += async (sender, args) =>
+			{
+				if (!args.Author.IsCurrent && args.Channel.Name == "691")
+				{
+					banned.RemoveAll(x => x.until < DateTimeOffset.UtcNow);
+
+					if (banned.Any(x => x.user == args.Author.Id))
+					{
+						await args.Message.DeleteAsync("You are still banned...");
+					}
+					else if (args.Message.Attachments.Count > 0)
+					{
+						int minutes = Random.Shared.Next(1, 59);
+
+						await args.Message.RespondAsync($"For making this post, this user was banned for {minutes} minutes");
+
+						banned.Add((args.Author.Id, DateTimeOffset.UtcNow.AddMinutes(minutes)));
+					}
+				}
+
+				await Task.CompletedTask;
+			};
 			#endregion
 
 			commands.RegisterCommands<VoteCommands>();
 
-            await discord.ConnectAsync();
+			await discord.ConnectAsync();
 
-            discord.Ready += async (client, args) =>
-            {
+			discord.Ready += async (client, args) =>
+			{
 				await discord.UpdateStatusAsync(
-                    new DiscordActivity("Rainbow Siege 6", ActivityType.Playing)
-                );
+					new DiscordActivity("Rainbow Siege 6", ActivityType.Playing)
+				);
 
 				await Task.CompletedTask;
 			};
 
-            await Task.Delay(-1);
-        }
-    }
+			await Task.Delay(-1);
+		}
+	}
 }
